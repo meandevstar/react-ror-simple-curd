27549cdcc533f3e6ff5cb3b9b4cded5b08a63bcd
meandevstar <aimoko1067@yahoo.com>
2017-06-21 19:32:32 +0800

    ADDED pre-checking if classroom exists for teacher on classroom column on teachers table

--- db/migrate/20170621020517_create_teachers.rb
+++ db/migrate/20170621020517_create_teachers.rb
@@ -2,7 +2,7 @@ class CreateTeachers < ActiveRecord::Migration[5.1]
   def change
     create_table :teachers do |t|
 
-    	t.references :classroom, index: true
+    	t.references :classroom, index: true, :null => true
     	t.column "name", :string, :null => false
     	t.column "gender", :string, :null => false
     	t.column "specialization", :string, :null => false
